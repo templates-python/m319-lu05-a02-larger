0f7902f81fa4ed907c419a4f15fe7a3fa20ea5a1
Marcel Suter <marcel.suter@bzz.ch>
2023-07-19 09:21:10 +0200

    initial commit

--- larger.py
+++ larger.py
@@ -1,6 +1,13 @@
 def main():
-    # Write your program here and remove the line "pass"
-    pass
+    number1 = int(input('Give the first number:\n'))
+    number2 = int(input('Give the second number:\n'))
+
+    if number1 > number2:
+        print('Greater number is: ' + str(number1))
+    elif number2 > number1:
+        print('Greater number is: ' + str(number2))
+    else:
+        print('The numbers are equal!')
 
 
 if __name__ == '__main__':
