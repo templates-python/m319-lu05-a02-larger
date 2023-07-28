6def963473fac736c554c8c2a70ceca94bc4ec66
Marcel Suter <marcel.suter@bzz.ch>
2023-07-28 14:12:04 +0200

    changed to print('f...')

--- larger.py
+++ larger.py
@@ -3,9 +3,9 @@ def main():
     number2 = int(input('Give the second number:\n'))
 
     if number1 > number2:
-        print('Greater number is: ' + str(number1))
+        print(f'Greater number is: {number1}')
     elif number2 > number1:
-        print('Greater number is: ' + str(number2))
+        print(f'Greater number is: {number2}')
     else:
         print('The numbers are equal!')
 
